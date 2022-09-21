d50ac276c077698188248f2086e75dad9f9c7caf
kkkaran <karanpreetsodhi1997@gmail.com>
2022-09-21 14:23:04 -0400

    edited app.js

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header({title,name}) {
+return (
+    <>
+        <div>{title}</div>
+        <h3>{name}</h3>dsds
+        
+        
+        {
+            //console.log(title)
+        }
+    </>
+    )
+}
